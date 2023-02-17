57cf059c965434a5be02dde443e85ed5b54ff3cd
SamAlcock <72557851+SamAlcock@users.noreply.github.com>
2023-02-17 16:05:41 +0000

    Checks .csv existing earlier so headers are written

--- CSVManager.cs
+++ CSVManager.cs
@@ -12,26 +12,24 @@ public class CSVManager : MonoBehaviour
     {
         ParticipantData = GetComponent<ChangeText>();
 
-        List<float> Data = new List<float>(ParticipantData.participant_data);
+        List<float> Data = new List<float>(ParticipantData.participant_data); // Take recorded data from task and store it into a list
 
-        string path = "leapfrog.csv";
+        string path = "leapfrog.csv"; // Location of .csv file
 
-        var participant = new List<ParticipantEntry>();
+        var participant = new List<ParticipantEntry>(); // Create a list of all of participants entries
 
-        for (int i = 1; i < Data.Count; i+=5)
+        bool exists = CheckIfFileExists(path); // Check if file exists - have to do this now instead of where the 'if (!exists)' is as the file already exists by then
+
+        for (int i = 1; i < Data.Count; i+=5) // Increment i by 5 because every 5 entries (excluding i = 0) is data for a new trial
         {
+            // Assign this data to a new instance of ParticipantEntry
             participant.Add(new ParticipantEntry { Id = (int)Data[0], Trial = (int)Data[i], Explored = (int)Data[i + 1], Exploited = (int)Data[i + 2], ResponseTime = Data[i + 3], TooSlow = (int)Data[i + 4]});
         }
 
-        for (int i = 0; i < participant.Count; i++) 
-        {
-            Debug.Log(participant[i].Id + "," + participant[i].Trial + "," + participant[i].Explored + "," + participant[i].Exploited + "," + participant[i].ResponseTime + "," + participant[i].TooSlow);
-        }
-
-        using FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write);
+        using FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write); // Allow appending to keep already recorded data
         using StreamWriter sw = new StreamWriter(fs);
  
-        if (!File.Exists(path))
+        if (!exists) // Create the column headings if the file didn't exist previously
         {
             sw.WriteLine("Participant Number,Trial Number,Explored,Exploited,Response Time (seconds),Too Slow");
         }
@@ -39,7 +37,7 @@ public class CSVManager : MonoBehaviour
         InputData(participant, sw);
     }
 
-    void InputData(List<ParticipantEntry> participant, StreamWriter writer)
+    void InputData(List<ParticipantEntry> participant, StreamWriter writer) // Inputs every instances attribute in the correct format
     {
         for (int i = 0; i < participant.Count; i++)
         {
@@ -47,7 +45,19 @@ public class CSVManager : MonoBehaviour
         }
     }
 
-    public class ParticipantEntry
+    bool CheckIfFileExists(string path) // Checks if file exists before writing 
+    {
+        if (File.Exists(path))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public class ParticipantEntry // Definitions for each entries attributes
     {
         public int Id { get; set; }
         public int Trial { get; set; }
